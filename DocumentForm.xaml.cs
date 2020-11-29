f4d695a91700d328a17aaaa74ea3adfa9b47767b
li-fstz <li-fstz@qq.com>
2020-11-29 17:22:12 +0800

    Code

---
+++ DocumentForm.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Controls;
+using ScintillaNET.WPF;
+using System.IO;
+using System.ComponentModel;
+using ScintillaNET;
+using System.Globalization;
+using System.Windows;
+using System.Diagnostics;
+using Xceed.Wpf.AvalonDock.Layout;
+
+namespace CPP_EP
+{
+    /// <summary>
+    /// Interaction logic for DocumentForm.xaml
+    /// </summary>
+    public partial class DocumentForm : LayoutDocument
+    {
+
+        public DocumentForm()
+        {
+            InitializeComponent ();
+            Title = "";
+            Scintilla.SavePointLeft += Scintilla_SavePointLeft;
+        }
+
+        private void Scintilla_SavePointLeft(object sender, EventArgs e)
+        {
+            AddOrRemoveAsteric();
+        }
+
+        public ScintillaWPF Scintilla
+        {
+            get { return scintilla; }
+        }
+
+        private string _filePath;
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { _filePath = value; }
+        }
+
+        private void AddOrRemoveAsteric()
+        {
+            if (scintilla.Modified)
+            {
+                if (!Title.EndsWith(" *", StringComparison.InvariantCulture))
+                    Title += " *";
+            }
+            else
+            {
+                if (Title.EndsWith(" *", StringComparison.InvariantCulture))
+                    Title = Title.Substring(0, Title.Length - 2);
+            }
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,324 @@
-﻿using System;
+﻿using CPP_EP.Execute;
+using CPP_EP.View;
+using ScintillaNET;
+using ScintillaNET.WPF;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CPP_EP {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow: Window {
+        private readonly MenuItem[] itemLabs;
+
+        private const int NUMBER_MARGIN = 2;
+
+        private const int BOOKMARK_MARGIN = 1;
+
+        private const int BOOKMARK_MARKER = 1;
+
+        private const int LINE_NUMBERS_MARGIN_WIDTH = 30; // TODO - don't hardcode this
+
+        private bool run = false;
+
+        private GDB gdb = null;
         public MainWindow () {
             InitializeComponent ();
+            itemLabs = new MenuItem[] { itemLab1, itemLab2, itemLab3, itemLab4, itemLab5, itemLab6, itemLab7, itemLab8 };
+        }
+        public DocumentForm ActiveDocument {
+            get { return documentsRoot.Children.FirstOrDefault (c => c.Content == dockPanel.ActiveContent) as DocumentForm; }
+        }
+        private void Window_Loaded (object sender, RoutedEventArgs e) {
+
+        }
+        private DocumentForm OpenFile (string filePath) {
+            DocumentForm doc = new DocumentForm();
+            SetScintillaToCurrentOptions (doc);
+            doc.Scintilla.Text = File.ReadAllText (filePath);
+            //doc.Scintilla.UndoRedo.EmptyUndoBuffer();
+            //doc.Scintilla.Modified = false;
+            doc.Title = Path.GetFileName (filePath);
+            doc.FilePath = filePath;
+            documentsRoot.Children.Add (doc);
+            doc.DockAsDocument ();
+            //doc.IsActive = true;
+            doc.CanClose = false;
+            doc.CanTogglePin = false;
+            //incrementalSearcher.Scintilla = doc.Scintilla;
+
+            return doc;
+        }
+        private void ItemLab_Click (object sender, RoutedEventArgs e) {
+            foreach (var itemLab in itemLabs) {
+                if (itemLab != sender) {
+                    itemLab.IsChecked = false;
+                } else {
+                    itemLab.IsChecked = true;
+                    Clean ();
+                    while (documentsRoot.Children.Count () != 0) {
+                        documentsRoot.Children[0].Close ();
+                    }
+                    if (sender == itemLab1) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab1.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\voidtable.c");
+                    } else if (sender == itemLab2) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab2.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\voidtable.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\first.c");
+                    } else if (sender == itemLab3) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab3.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\voidtable.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\first.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\follow.c");
+                    } else if (sender == itemLab4) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab4.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\voidtable.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\first.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\follow.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\parsingtable.c");
+                    } else if (sender == itemLab5) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab5.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\pickupleftfactor.c");
+                    } else if (sender == itemLab6) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab6.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\removeleftrecursion1.c");
+                    } else if (sender == itemLab7) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab7.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\removeleftrecursion2.c");
+                    } else if (sender == itemLab8) {
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\lab8.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\rule.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\voidtable.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\first.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\follow.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\parsingtable.c");
+                        OpenFile ("C:\\Users\\li-fs\\Documents\\labs\\src\\inc\\parser.c");
+                    }
+                }
+            }
+            
+        }
+        private void SetScintillaToCurrentOptions (DocumentForm doc) {
+            ScintillaWPF ScintillaNet = doc.Scintilla;
+            //ScintillaNet.KeyDown += ScintillaNet_KeyDown;
+
+            // INITIAL VIEW CONFIG
+            ScintillaNet.WrapMode = WrapMode.None;
+            ScintillaNet.IndentationGuides = IndentView.LookBoth;
+
+            // STYLING
+            InitSyntaxColoring (ScintillaNet);
+
+            // NUMBER MARGIN
+            InitNumberMargin (ScintillaNet);
+
+            // BOOKMARK MARGIN
+            InitBookmarkMargin (ScintillaNet);
+
+            doc.Scintilla.WrapMode = WrapMode.Word;
+
+
+            ScintillaNet.CaretLineVisible = true;
+            ScintillaNet.CaretLineBackColor = System.Windows.Media.Color.FromRgb (0, 0, 100);
+            ScintillaNet.CaretLineBackColorAlpha = 100;
+
+            
+            // Set the zoom
+            //doc.Scintilla.Zoom = _zoomLevel;
+        }
+        private void InitSyntaxColoring (ScintillaWPF ScintillaNet) {
+            // Configure the default style
+            ScintillaNet.StyleResetDefault ();
+            ScintillaNet.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            ScintillaNet.Styles[ScintillaNET.Style.Default].Size = 10;
+            ScintillaNet.StyleClearAll ();
+
+            // Configure the CPP (C#) lexer styles
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Default].ForeColor = Color.Silver;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Comment].ForeColor = Color.FromArgb (0, 128, 0); // Green
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentLine].ForeColor = Color.FromArgb (0, 128, 0); // Green
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb (128, 128, 128); // Gray
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Number].ForeColor = Color.Olive;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = Color.Blue;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = Color.DarkCyan;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.String].ForeColor = Color.FromArgb (163, 21, 21); // Red
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Character].ForeColor = Color.FromArgb (163, 21, 21); // Red
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Verbatim].ForeColor = Color.FromArgb (163, 21, 21); // Red
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.StringEol].BackColor = Color.Pink;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Operator].ForeColor = Color.Purple;
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+
+            ScintillaNet.Lexer = Lexer.Cpp;
+
+            ScintillaNet.SetKeywords (0, "NULL break case const continue default do else enum extern for goto if long return signed sizeof static struct switch typedef union unsigned while char double float int long short void");
+            ScintillaNet.SetKeywords (1, "Rule RuleSymbol Set SetList ParsingStack VoidTable pHead pRule pSelect pSymbol");
+        }
+        private void InitNumberMargin (ScintillaWPF ScintillaNet) {
+            var nums = ScintillaNet.Margins[NUMBER_MARGIN];
+            nums.Width = LINE_NUMBERS_MARGIN_WIDTH;
+            nums.Type = MarginType.Number;
+            nums.Sensitive = true;
+            nums.Mask = 0;
+
+        }
+        private void InitBookmarkMargin (ScintillaWPF ScintillaNet) {
+            var margin = ScintillaNet.Margins[BOOKMARK_MARGIN];
+            margin.Width = 16;
+            margin.Sensitive = true;
+            margin.Type = MarginType.Symbol;
+            margin.Mask = (1 << BOOKMARK_MARKER);
+
+            var marker = ScintillaNet.Markers[BOOKMARK_MARKER];
+            marker.Symbol = MarkerSymbol.Circle;
+            marker.SetBackColor (IntToColor (0xFF003B));
+            marker.SetForeColor (IntToColor (0x000000));
+            marker.SetAlpha (100);
+            ScintillaNet.MarginClick += TextArea_MarginClick;
+        }
+        private void TextArea_MarginClick (object sender, MarginClickEventArgs e) {
+            ScintillaWPF TextArea = ActiveDocument.Scintilla;
+            if (e.Margin == BOOKMARK_MARGIN) {
+                const uint mask = (1 << BOOKMARK_MARKER);
+                Line line;
+                if (gdb != null) {
+                    var b = gdb.SetBreakpoint (ActiveDocument.Title + ":" + (TextArea.LineFromPosition(e.Position) + 1));
+                    line = TextArea.Lines[b.Line - 1];
+                } else {
+                    line = TextArea.Lines[TextArea.LineFromPosition (e.Position)];
+                }
+                // Do we have a marker for this line?
+                if ((line.MarkerGet () & mask) > 0) {
+                    // Remove existing bookmark
+                    line.MarkerDelete (BOOKMARK_MARKER);
+                } else {
+                    // Add bookmark
+                    line.MarkerAdd (BOOKMARK_MARKER);
+                }
+            }
+        }
+        public static Color IntToColor (int rgb) {
+            return Color.FromArgb (255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+        }
+
+        private void startButton_Click (object sender, RoutedEventArgs e) {
+            startButton.IsEnabled = false;
+            if (run) {
+                AfterRun (gdb.Continue ());
+            } else {
+                Clean ();
+                for (int i = 0; i < itemLabs.Count (); i++) {
+                    if (itemLabs[i].IsChecked) {
+                        new Xmake ("C:\\Users\\li-fs\\Documents\\labs", "D:\\xmake\\xmake.exe", i + 1).Build (buildText);
+                        gdb = new GDB ("D:\\MinGW\\bin\\gdb.exe", "C:\\Users\\li-fs\\Documents\\labs\\build\\lab" + (i + 1) + ".exe", gdbText);
+                        run = true;
+                        SetBreakpoint ();
+                        AfterRun (gdb.Run ());
+                        break;
+                    }
+                }
+            }
+            startButton.IsEnabled = true;
+        }
+
+        private void SetBreakpoint() {
+            foreach (DocumentForm doc in documentsRoot.Children) {
+                int line = 0;
+                while (true) {
+                    line = doc.Scintilla.Lines[line + 1].MarkerNext (1 << BOOKMARK_MARKER);
+                    if (line == -1) {
+                        break;
+                    } else {
+                        var r = gdb.SetBreakpoint (doc.Title, line + 1);
+                        if (r.Line != line + 1) {
+                            Console.WriteLine (line);
+                            doc.Scintilla.Lines[line].MarkerDelete (BOOKMARK_MARKER);
+                            doc.Scintilla.Lines[r.Line - 1].MarkerAdd (BOOKMARK_MARKER);
+                        }
+                    }
+                }
+            }
+        }
+        private void AfterRun(RunResult r) {
+            stepButton.IsEnabled = false;
+            nextButton.IsEnabled = false;
+            finishButton.IsEnabled = false;
+            stopButton.IsEnabled = true;
+            if (r.Running) {
+
+            } else {
+                if (r.Reason == "breakpoint-hit" || r.Reason == "end-stepping-range" || r.Reason == "function-finished") {
+                    var rv = new RuleView(gdb);
+                    var vv = new VoidTableView(gdb);
+                    var fsv = new FirstView(gdb);
+                    var flv = new FollowView(gdb);
+                    var ptv = new ParsingTableView(gdb);
+                    List<RuleView.Rule> rules = rv.GetRules("pHead");
+                    VoidTableView.VoidTable voidTable = vv.GetVoidTable("&VoidTable");
+                    List<FirstView.Set> firstSet = fsv.GetSetList ("&FirstSetList");
+                    List<FirstView.Set> followSet = flv.GetSetList ("&FollowSetList");
+                    List<ParsingTableView.Set> selectSet = ptv.GetSetList ("&SelectSetList");
+                    ParsingTableView.ParsingTable parsingTable = ptv.GetParsingTable ("&ParsingTable");
+                    foreach (DocumentForm doc in documentsRoot.Children) {
+                        if (doc.Title == Path.GetFileName (r.Breakpoint.Filename)) {
+                            doc.IsActive = true;
+                            doc.Scintilla.Lines[r.Breakpoint.Line - 1].Goto ();
+                            doc.Scintilla.Scintilla.Focus ();
+
+                            stepButton.IsEnabled = true;
+                            nextButton.IsEnabled = true;
+                            finishButton.IsEnabled = true;
+                            break;
+                        }
+                    }
+                } else {
+                    run = false;
+                    Console.WriteLine (r.Reason);
+                }
+            }
+        }
+
+        private void stepButton_Click (object sender, RoutedEventArgs e) {
+            AfterRun (gdb.Step ());
+        }
+
+        private void nextButton_Click (object sender, RoutedEventArgs e) {
+            AfterRun (gdb.Next());
+        }
+
+        private void finishButton_Click (object sender, RoutedEventArgs e) {
+            AfterRun (gdb.Finish());
+        }
+
+        private void Clean() {
+            if (gdb != null) {
+                gdb.Stop ();
+                gdb = null;
+            }
+            run = false;
+            startButton.IsEnabled = true;
+            refButton.IsEnabled = false;
+            stopButton.IsEnabled = false;
+            stepButton.IsEnabled = false;
+            nextButton.IsEnabled = false;
+            finishButton.IsEnabled = false;
+        }
+
+        private void stopButton_Click (object sender, RoutedEventArgs e) {
+            Clean ();
         }
     }
 }
