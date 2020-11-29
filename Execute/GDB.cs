4ce96d7261f7891143239887effc8a950c93d0ef
li-fstz <li-fstz@qq.com>
2020-11-29 17:19:34 +0800

    GDB 操作

--- Execute/GDB.cs
+++ Execute/GDB.cs
@@ -1,18 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace CPP_EP.Execute {
     using ReciveData = List<List<string>>;
-    class GDB: Execute {
-        public GDB (string gdbpath, string filepath) : base (gdbpath, "-silent --interpreter mi " + filepath) {
+    class GDB {
+        class ExecuteNotResponseException: Exception { }
+        private Process ExecuteProcess;
+        private Thread ReadThread;
+        private readonly string Filename;
+        private readonly string Arguments;
+        private readonly Queue<List<string>> reciveData = new Queue<List<string>>();
+        private static TextBox GDBText;
+        public GDB (string gdbpath, string filepath, TextBox gdbText){
+            GDBText = gdbText;
+
+            Filename = gdbpath;
+            Arguments = "-silent --interpreter mi " + filepath;
+
+            ExecuteProcess = new Process ();
+            ExecuteProcess.StartInfo.FileName = Filename;
+            ExecuteProcess.StartInfo.Arguments = Arguments;
+            ExecuteProcess.StartInfo.UseShellExecute = false;
+            ExecuteProcess.StartInfo.RedirectStandardOutput = true;
+            ExecuteProcess.StartInfo.RedirectStandardInput = true;
+            ExecuteProcess.StartInfo.RedirectStandardError = true;
+            ExecuteProcess.StartInfo.CreateNoWindow = true;
+            ExecuteProcess.Start ();
+            ExecuteProcess.StandardInput.AutoFlush = true;
+            ReadThread = new Thread (new ThreadStart (Receive));
+            ReadThread.Start ();
             Send ("set args > out.txt");
             Send ("set print null-stop on");
-            GetReciveData ().ForEach (l => ParseResponcs (l));
+            GetReciveData (200).ForEach (l => ParseResponcs (l));
+
         }
         private RunResult SomeWayExecute (string cmd) {
             Send (cmd);
-            return RunResult.Parse (GetReciveData ());
+            return RunResult.Parse (GetReciveData (200));
         }
         public RunResult Run () {
             return SomeWayExecute ("-exec-run");
@@ -47,17 +75,23 @@ namespace CPP_EP.Execute {
             return ParseResponcs (GetReciveData ()[0]).Result.Class == "done";
         }
         public string Print (string parameter) {
-            Send ("print " + parameter);
-            return ParseResponcs (GetReciveData ()[0]).StreamConsole;
+            Send ("-data-evaluate-expression \"" + parameter + "\"");
+            var r = ParseResponcs (GetReciveData ()[0]);
+            if (r.Result.Class == "done") {
+                return r.Result.Result;
+            } else {
+                return null;
+            }
         }
         public bool ClearBreakpoint (string filename, int line) {
             BreakPoint.Delete (filename, line);
-            return ClearBreakpoint (String.Format ("{0}:{1}", filename, line));
+            return ClearBreakpoint (string.Format ("{0}:{1}", filename, line));
         }
         public static GDBOutput ParseResponcs (List<string> Responcs) {
             GDBOutput output = new GDBOutput();
             Responcs.ForEach (s => {
-                Console.WriteLine ("  *  " + s);
+                Console.WriteLine ("gdb -> " + s);
+                GDBText.AppendText ("gdb -> " + s + "\n");
                 int offset;
                 switch (s.ToCharArray ()[0]) {
                     case '*':
@@ -93,6 +127,56 @@ namespace CPP_EP.Execute {
             });
             return output;
         }
+        protected void Send (string cmd) {
+            Console.WriteLine ("gdb <- " + cmd);
+            GDBText.AppendText ("gdb <- " + cmd + "\n");
+            ExecuteProcess.StandardInput.WriteLine (cmd);
+        }
+        protected ReciveData GetReciveData (int time = 10) {
+            Thread.Sleep (time);
+            bool r = false;
+            ReciveData data = new ReciveData();
+            lock (reciveData) {
+                while (reciveData.Count != 0) {
+                    r = true;
+                    data.Add (reciveData.Dequeue ());
+                }
+            }
+            if (r) {
+                return data;
+            } else {
+                return GetReciveData ();
+            }
+        }
+        public void Stop () {
+            ReadThread.Abort ();
+            ExecuteProcess.Kill ();
+            ReadThread = null;
+            ExecuteProcess = null;
+        }
+        private void Receive () {
+            string s;
+            List<string> tmp = null;
+            try {
+                while ((s = ExecuteProcess.StandardOutput.ReadLine ()) != null) {
+                    if (tmp == null) {
+                        Monitor.Enter (reciveData);
+                        tmp = new List<string> ();
+                    }
+                    if (s == "(gdb) ") {
+                        reciveData.Enqueue (tmp);
+                        Monitor.Exit (reciveData);
+                        tmp = null;
+                    } else {
+                        tmp.Add (s);
+                    }
+                }
+            } catch (Exception) { }
+            if (tmp != null) {
+                reciveData.Enqueue (tmp);
+                Monitor.Exit (reciveData);
+            }
+        }
     }
     class GDBOutput {
         public class Record {
@@ -127,6 +211,7 @@ namespace CPP_EP.Execute {
             BreakPoint b = Get(filename, line);
             if (b == null) {
                 b = new BreakPoint (filename, line);
+                BreakPoints.Add (b);
             }
             return b;
         }
@@ -177,9 +262,9 @@ namespace CPP_EP.Execute {
             } else {
                 string reason = StopReason.Match (result).Groups[1].Value;
                 BreakPoint breakPoint = null;
-                if (reason == "breakpoint-hit") {
+                if (reason == "breakpoint-hit" || reason == "end-stepping-range" || reason == "function-finished") {
                     (var filename, var line) = BreakPoint.Parsing (result);
-                    breakPoint = BreakPoint.Get (filename, line);
+                    breakPoint = BreakPoint.Insert (filename, line);
                 }
                 r = new RunResult () {
                     Running = running,
